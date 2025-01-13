e88d5e51926cc5650a73bd904d45d68e6e4223e0
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-01-13 10:40:37 -0500

    Added the printing of env variables forgot to push this earlier

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const { port } = require('../src/configuration');
+const { port, log_level } = require('../src/configuration');
 const stoppable = require('stoppable');
 const logger = require('../src/logger');
 const app = require('../src/app');
@@ -6,8 +6,13 @@ const app = require('../src/app');
 // Default the port to 8080
 const server_port = parseInt(port || '8080', 10);
 
+const printENVVariables = () => {
+  logger.debug(`ENV_Port: ${port}\nLog_Level: ${log_level}`);
+};
+
 const server = stoppable(
   app.listen(server_port, () => {
+    printENVVariables();
     logger.info(`Server started on port: ${port}`);
   })
 );
