d7a8d5c91a0c93fb06127ce502a3f0f3b7324f0a
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-02-10 14:12:56 -0500

    added memory storage configuration

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,1 +1,11 @@
-module.exports = require('./memory');
+if (process.env.USE_IN_MEMORY && process.env.USE_AWS_MEMORY) {
+  throw new Error('env contains configuration for both AWS  and In Memory. Only one is allowed.');
+}
+
+if (process.env.USE_AWS_MEMORY == 'true') {
+  module.exports = require('./aws');
+} else if (process.env.USE_IN_MEMORY == 'true') {
+  module.exports = require('./memory');
+} else {
+  throw new Error('missing env vars: no memory strategy configuration found');
+}
