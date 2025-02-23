c54ac4264e12df22114813be5819cf99d89af514
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-02-23 14:58:46 -0500

    Added GET Fragments?expand=1 from assignment 2 functionality

--- src/routes/api/get/get.js
+++ src/routes/api/get/get.js
@@ -8,9 +8,15 @@ const getFragments = async (req, res) => {
   const email = req.user?.email || req.headers.authorization?.split(' ')[1]?.split(':')[0];
 
   const ownerId = crypto.createHash('sha256').update(email).digest('hex');
-  const data = { fragments: await Fragment.byUser(ownerId, true) };
-  const success = createSuccessResponse(data);
+  const expand = req.query?.expand;
+  logger.debug(`req.query: ${req.query.expand}`);
 
+  const data =
+    expand == 1
+      ? { fragments: await Fragment.byUser(ownerId, true) }
+      : { fragments: await Fragment.byUser(ownerId, false) };
+
+  const success = createSuccessResponse(data);
   return res.status(200).json(success);
 };
 
